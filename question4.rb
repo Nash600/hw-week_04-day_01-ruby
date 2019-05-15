29bb94be49e0a30ee8186c48f3b862a2e98e3d2c
Nash600 <nashat6000@gmail.com>
2019-05-14 21:50:06 -0400

    solved q4 and deleted duplicate files

---
+++ question4.rb
@@ -0,0 +1,56 @@
+stores = [
+  {
+      store_name: 'Jarir',
+      products: [
+          {
+              description: 'Titanium',
+              price: 9384.33
+          },
+          {
+              description: 'Gold',
+              price: 345.54
+          }
+      ]
+  },
+  {
+      store_name: 'Danub',
+      products: [
+          {
+              description: 'Silver',
+              price: 654.44
+          },
+          {
+              description: 'Ruby',
+              price: 323.43
+          }
+      ]
+  },
+  {
+      store_name: 'Souq',
+      products: [
+          {
+              description: 'Opal',
+              price: 345.43
+          },
+          {
+              description: 'Sapphire',
+              price: 899.33
+          }
+      ]
+  }
+]
+
+most_expensive_products_by_store = []
+
+most_expensive_products_by_store = stores.map do |store|
+    most_expensive = store[:products].max_by do |product|
+        product[:price]
+    end  
+
+    {
+       name: store[:store_name] ,
+       price: most_expensive[:price]
+    }
+end 
+
+puts most_expensive_products_by_store
